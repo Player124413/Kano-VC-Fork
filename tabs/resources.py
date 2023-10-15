01216d403599c09f373a7d7c9179e9a99d0ffb49
STaYsONzxc <80705216+STaYsONzxc@users.noreply.github.com>
2023-10-15 14:21:51 +0700

    Update resources.py

--- tabs/resources.py
+++ tabs/resources.py
@@ -319,9 +319,27 @@ def download_from_url(url):
                     return None
             else:
                 return None
+        elif "disk.yandex.ru" in url:
+            import requests
+            from urllib.parse import urlencode
+
+            base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
+            public_key = url  # Сюда вписываете вашу ссылку
+
+            # Получаем загрузочную ссылку
+            final_url = base_url + urlencode(dict(public_key=public_key))
+            response = requests.get(final_url)
+            download_url = response.json()['href']
+
+            # Загружаем файл и сохраняем его
+            download_response = requests.get(download_url)
+            file_name = url.split('/')[-1] + '.zip'  # Извлекаем имя файла из URL
+            with open(os.path.join(zips_path, file_name), 'wb') as f:   # Здесь укажите нужный путь к файлу
+                f.write(download_response.content)
         else:
             os.chdir(zips_path)
             wget.download(url)
+
 
         # Fix points in the zips
         for currentPath, _, zipFiles in os.walk(zips_path):
